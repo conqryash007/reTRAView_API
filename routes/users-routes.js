ee5cd9f80a7dda1f3a305572bd14a1c96a270058
conqryash007 <guptayash200010@gmail.com>
2021-12-05 20:31:04 +0530

    completed api developmemt for reTRAView

---
+++ routes/users-routes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+const usersController = require("./../controllers/users-controller");
+const { check } = require("express-validator");
+
+router.get("/", usersController.getUsers);
+
+router.post(
+  "/signup",
+  [
+    check("name").not().isEmpty(),
+    check("email").normalizeEmail().isEmail(),
+    check("password").isLength({ min: 5 }),
+  ],
+  usersController.signUp
+);
+
+router.post("/login", usersController.logIn);
+
+module.exports = router;
