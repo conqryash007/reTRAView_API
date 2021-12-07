cae7b3f1af948caf86a30963a20a90309f38baec
conqryash007 <guptayash200010@gmail.com>
2021-12-07 15:49:15 +0530

    configured multer file uplaod

---
+++ middleware/file-upload.js
@@ -0,0 +1,27 @@
+const multer = require("multer");
+const { v1: uuidv1 } = require("uuid");
+const MIME_TYPE_MAP = {
+  "image/png": "png",
+  "image/jpg": "jpg",
+  "image/jpeg": "jpeg",
+};
+
+const fileupload = multer({
+  limit: 500000,
+  storage: multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "uploads/images");
+    },
+    filename: (req, file, cb) => {
+      const exten = MIME_TYPE_MAP[file.mimetype];
+      cb(null, uuidv1() + "." + exten);
+    },
+    filefilter: (req, file, cb) => {
+      const isValid = !!MIME_TYPE_MAP[file.mimetype];
+      let error = isValid ? null : new Error("Invaid file type");
+      cb(error, isValid);
+    },
+  }),
+});
+
+module.exports = fileupload;
--- routes/users-routes.js
+++ routes/users-routes.js
@@ -2,11 +2,13 @@ const express = require("express");
 const router = express.Router();
 const usersController = require("./../controllers/users-controller");
 const { check } = require("express-validator");
+const fileupload = require("./../middleware/file-upload");
 
 router.get("/", usersController.getUsers);
 
 router.post(
   "/signup",
+  fileupload.single("image"),
   [
     check("name").not().isEmpty(),
     check("email").normalizeEmail().isEmail(),
